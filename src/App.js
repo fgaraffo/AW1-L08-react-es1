cb3f9f2551744b9fc26f24e7244c794e570e55ec
flg <pluton3@icloud.com>
2021-06-07 17:36:24 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import { Col, Container, Row } from 'react-bootstrap';
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WelcomeButton />
+      <p></p>
+      <Counter initialCount={7} />
+      <p></p>
+      <ShortText text='This is a long long text' maxLength={10} />
     </div>
+  );
+}
+
+function WelcomeButton(props) {
+  const [english, setEnglish] = useState(true);
+  const toggleLanguage = () => { setEnglish(e => !e) };
+  /*return (<button onClick={ () => setEnglish( eng => !eng )}></button>)*/;
+  return (<button onClick={toggleLanguage}>
+    {english ? 'Hello' : 'Ciao'}
+  </button>);
+}
+
+//funziona anche con props e props.initialCount, così prendi solo la proprietà che interessa,
+//in questo caso è anche l'unica passata
+function Counter({ initialCount }) {
+  const [count, setCount] = useState(initialCount);
+  return (
+    <>
+      Count: {count}<br />
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+    </>
+  );
+}
+
+function ShortText(props) {
+  const [hidden, setHidden] = useState(true);
+  return (
+    <span>
+      {hidden ? `${props.text.substr(0, props.maxLength)}...` : props.text }
+      {hidden ? (
+        <a href='#' onClick={ () => setHidden(false) }>more</a>
+      ) : (
+        <a href='#' onClick={ () => setHidden(true) }>less</a>
+      )}
+    </span>
   );
 }
 
